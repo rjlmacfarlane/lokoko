0bc0de2a3c3230991f01eacdb929d67899257ffb
Irena-Pod <irena.podgurski@gmail.com>
2021-01-10 22:28:04 +0000

    remove favourited column from db and update seeds

--- db/seeds/01_seeds.sql
+++ db/seeds/01_seeds.sql
@@ -12,8 +12,8 @@ INSERT INTO users (name, email, province, city, phone_number, profile_picture) V
 INSERT INTO listings (title, description, thumbnail_photo_url, main_photo_url, price, condition, posted_date, sold_date, category_id, user_id)
 VALUES
 ('Computer chair', 'blue color', 'https://i.imgur.com/QR2RdTgt.jpg', 'https://i.imgur.com/QR2RdTg.jpg', 30, 'Used', '2008-01-09 15:45:21', NULL, 4, 1),
-('Sennheiser headphones', 'black color, 300 ohms', 'https://i.imgur.com/tNuze0j.jpg', 'https://i.imgur.com/tNuze0t.jpg', 200, 'New', '2008-03-09 15:45:21', NULL, 3, 3),
-('Barber Chair', 'black color', 'https://i.redd.it/ewkikofclok41.jpg', 'https://i.redd.it/ewkikofclok41.jpg', 100, 'Used', '2008-03-09 15:45:21', NULL, 4, 4),
+('Sennheiser headphones', 'black color, 300 ohms', 'https://i.imgur.com/tNuze0j.jpg', 'https://i.imgur.com/tNuze0t.jpg', 200, 'New', '2008-03-09 15:45:21', NULL, 3, 1),
+('Barber Chair', 'black color', 'https://i.redd.it/ewkikofclok41.jpg', 'https://i.redd.it/ewkikofclok41.jpg', 100, 'Used', '2008-03-09 15:45:21', NULL, 4, 2),
 ('2004 Pontiac Aztek', 'Green automatic, new windshield!', 'http://pics.imcdb.org/15489/breakingbads05e04720phdtvx264-evolvemkv_000107064.jpg', 'http://pics.imcdb.org/15489/breakingbads05e04720phdtvx264-evolvemkv_000107064.jpg', 50, 'Used', '2005-03-09 15:45:21', NULL, 1, 6),
 ('Bagpipes', 'Tartan, only used once to get myself fired.', 'https://www.wdiy.org/sites/wdiy/files/styles/medium/public/201707/pipes.jpg', 'https://www.wdiy.org/sites/wdiy/files/styles/medium/public/201707/pipes.jpg', 250, 'Used', '2006-03-09 15:45:21', NULL, 5, 7),
 ('Falcon 9 Rocket', 'Launched seven times. Inserted 311 Starlink satellites into orbit.', 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Bangabandhu_Satellite-1_Mission_(42025499722).jpg/270px-Bangabandhu_Satellite-1_Mission_(42025499722).jpg', 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Bangabandhu_Satellite-1_Mission_(42025499722).jpg/270px-Bangabandhu_Satellite-1_Mission_(42025499722).jpg', 90000000, 'Used', '2020-05-05 13:30:21', NULL, 6, 5),
@@ -27,6 +27,6 @@ INSERT INTO listing_categories (name) VALUES
   ('Musical Instruments'),
   ('Other');
 
-INSERT INTO favourite_listings (favourited, created_date, user_id, listing_id) VALUES
-(true, '2008-03-10 15:45:21', 1, 3),
-(true, '2008-05-12 09:45:21', 3, 2);
+INSERT INTO favourite_listings (created_date, user_id, listing_id) VALUES
+('2008-03-10 15:45:21', 1, 3),
+('2008-05-12 09:45:21', 3, 2);
